d6f4a38e364c370a5e2f53e766ec40ae029413a7
JBallou-1 <54325784+JBallou-1@users.noreply.github.com>
2021-02-11 23:23:49 -0500

    Added rotation state saving and fixed issue with buttons closing app.

--- app/src/main/java/com/example/button_counter_ballou/MainActivity.java
+++ app/src/main/java/com/example/button_counter_ballou/MainActivity.java
@@ -1,17 +1,8 @@
 package com.example.button_counter_ballou;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,49 +20,34 @@ private int count;
         incrementorB = (Button) this.findViewById(R.id.incrementorB);
         decrementorB = (Button) this.findViewById(R.id.decrementorB);
         resetB = (Button) this.findViewById(R.id.resetB);
+        count = 0;
         incrementorB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count += 1;
-                text.setText(count);
+                text.setText(String.valueOf(count));
             }
         });
         decrementorB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count -= 1;
-                text.setText(count);
+                text.setText(String.valueOf(count));
             }
         });
         resetB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count = 0;
-                text.setText(count);
+                text.setText(String.valueOf(count));
             }
         });
         text = (TextView) this.findViewById(R.id.text);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
